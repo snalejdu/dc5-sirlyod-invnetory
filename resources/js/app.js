0b80adaee7ab145b7b4c5e6173ab9a3cf89809e0
snalejdu <karlfritzpagas4@gmail.com>
2026-04-01 13:41:03 +0800

    first commit

---
+++ resources/js/app.js
@@ -0,0 +1,16 @@
+import './bootstrap';
+import { createApp, h } from 'vue'
+import { createInertiaApp } from '@inertiajs/vue3'
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers'
+import { ZiggyVue } from 'ziggy-js'
+
+createInertiaApp({
+    title: (title) => `${title} - Inventory App`,
+    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
+    setup({ el, App, props, plugin }) {
+        return createApp({ render: () => h(App, props) })
+            .use(plugin)
+            .use(ZiggyVue)
+            .mount(el)
+    },
+})
